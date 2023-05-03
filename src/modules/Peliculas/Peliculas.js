7eebfdd9e2ec1e00a48e22775574d263b1cceb02
JcalleA <kalle-1413@hotmail.com>
2023-05-03 17:45:03 -0500

    ok

--- src/modules/Peliculas/Peliculas.js
+++ src/modules/Peliculas/Peliculas.js
@@ -1,13 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import "./Peliculas.css";
-
+import { ImExit } from "react-icons/im"
 const Peliculas = () => {
 
-
-
-
-    const keyApi = '?api_key=01f8864c658ff852bda51d8e300d91de';
+    const keyApi = '?api_key=01f8864c658ff852bda51d8e300d91de&language=es-ES';
     const baseUrl = 'https://api.themoviedb.org/3';
     const imagePath = 'https://image.tmdb.org/t/p/original';
     const search = '/search/movie';
@@ -18,21 +15,41 @@ const Peliculas = () => {
     const urlTmdb = baseUrl + discover + keyApi;
     const yts = "https://yts.mx/api/v2/list_movies.json";
     const consulta2 = "?query_term=";
-    const uri ="http://localhost:4000/1080/Video"; //"https://back-movies-jwyg.onrender.com/1080/Video";
-    const external = baseUrl+"/find/tt10151854"+keyApi+"&external_source=imdb_id";
+    const uri = "http://localhost:4000/"; //"https://back-movies-jwyg.onrender.com/1080/Video";
+    const external = baseUrl + "/find/tt10151854" + keyApi + "&external_source=imdb_id";
+    const display = {
+        display: "none"
+    };
+
+    const colorRef = useRef();
+    const ver = () => {
+        colorRef.current.style.display = 'block';
+    };
 
 
     const getPeliculas = async (page) => {
-        
-        const datos = await axios.get(uri);
-        setDatos(datos.data);
-        console.log(datos.data);
-// category:"207"  id:"55131929"   imdb: ""
-// magnet:"magnet:?xt=urn:btih:CF109D8D0CFE46BFE7AC5378B587D27B71DD87A8&dn=The%20Matrix%20Resurrections%20(2021)%20%5B1080p%5D%20%5BWEBRip%5D&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce"
-// numFiles: 8 peers: 29   provider: "ThePirateBay"    seeds: 473
-// size: "2.9 GB"  status: "vip"   time : "Wed, 22 Dec 2021 13:59:13 GMT"
-// title:"The Matrix Resurrections (2021) [1080p] [WEBRip]"
 
+        const { data } = await axios.get(urlTmdb);
+        let lis = [];
+
+        data.results.forEach(element => {
+            lis.push(axios.get(uri + element.title))
+        });
+        const promises = await axios.all(lis);
+        let listaFull = [];
+        let n = 0;
+        data.results.forEach((item) => {
+            item.torrent = promises[n].data[0].magnet;
+            listaFull.push(item);
+            console.log(listaFull);
+            n = n + 1;
+        })
+        setDatos(listaFull);
+        console.log(datos1);
+
+    }
+
+    const salir = (id) => {
 
     }
 
@@ -72,9 +89,28 @@ const Peliculas = () => {
                             <div className="titleCont">
                                 <h1 >{element.title}</h1>
                             </div>
-                            <div className="imgCont">
-                                <img className="imgPeli" src={`${element.medium_cover_image
-                                    }`}></img>
+                            <div className="imgCont" onClick={ver}>
+                                <img className="imgPeli" src={`${imagePath + element.poster_path}`}></img>
+                            </div>
+                        </div>
+                        <div className="modal" ref={colorRef}>
+                            <div className="modalCont" >
+                                <div className="modalIconCont">
+                                    <ImExit onClick={salir(`${element.id}`)} />
+                                </div>
+                                <div className="modalImgCont">
+                                    <img className="modalImg" src={`${imagePath + element.poster_path}`}></img>
+                                </div>
+                                <div className="modalListCont">
+                                    <ol>
+                                        <li>Rate: {element.vote_average}</li>
+                                        <li>Total Votos: {element.vote_count}</li>
+                                        <li>Lanzamiento: {element.release_date}</li>
+                                    </ol>
+                                </div>
+                                <div className="modalParrafo">
+                                    <p>Descripcion: <br></br>{element.overview}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
